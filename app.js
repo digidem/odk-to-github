1912fac191ef020a5c8cafa3deb57df9d994095e
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-01-25 10:58:18 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app     = express();
+var Github = require('github-api');
+var odk_token = process.env.ODK_WAPICHANA_TOKEN;
+var github_token = process.env.GITHUB_WAPICHANA_TOKEN;
+
+app.use(express.json());
+
+var github =  new Github({token: github_token, auth: "oauth"});
+
+// Receive webhook post
+app.post('/:user/:repo/:branch', function(req, res) {
+
+    var data = req.body;
+
+    if (data.token !== odk_token) {
+        res.send(403);
+        return;
+    }
+
+    // Close connection
+    res.send(202);
+
+    var repo = github.getRepo(req.params.user, req.params.repo);
+    var branch = req.params.branch;
+    var uuid = data.data.instanceID.replace(/^uuid:/, "");
+    var filename = data.formId + "/" + uuid + ".json";
+
+    repo.write(branch, filename, JSON.stringify(data), "Added new form response " + uuid, function(err) { 
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("commited form instance " + uuid);
+        } 
+    });
+
+});
+
+// Start server
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log('Listening on port ' + port);
